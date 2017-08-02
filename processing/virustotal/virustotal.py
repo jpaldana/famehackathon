58e7d2080ca817fdef7c19cb417c5beb86010939
James Luong <jluong@godaddy.com>
2017-08-01 23:38:46 -0700

    Adding virustotal module for testing.

---
+++ processing/virustotal/virustotal.py
@@ -0,0 +1,60 @@
+import os
+import time
+from urlparse import urljoin
+from urllib import urlopen
+
+try:
+    import virustotal
+    HAVE_VIRUSTOTAL = True
+except ImportError:
+    HAVE_VIRUSTOTAL = False
+
+from fame.common.utils import tempdir
+from fame.common.exceptions import ModuleInitializationError, ModuleExecutionError
+from fame.core.module import ProcessingModule
+
+
+class VirusTotal(ProcessingModule):
+    name = ""
+    description = "This is broken. So bad. So.... bad...."
+    acts_on = ["executable", "word", "html", "rtf", "excel", "pdf", "javascript", "jar", "url", "powerpoint", "vbs"]
+
+    config = [
+        {
+            'name': 'api-key',
+            'type': 'str',
+            'default': 'Meowmix',
+            'description': "API Key for VirusTotal"
+        }
+    ]
+
+    def initialize(self):
+        # Check dependencies
+        if not HAVE_VIRUSTOTAL:
+            raise ModuleInitializationError(self, "Missing dependency: virustotal")
+
+    def each(self, target):
+        v = virustotal.VirusTotal(self.api_key)
+        vt_report = v.scan(target)
+        vt_report.join()
+        assert vt_report.done is True
+        self.report.sha256 = vt_report.sha256
+        self.report.hit_ratio = vt_report.positives / vt_report.total
+        self.report.vt_scan_uid = vt_report.scan
+       	positive_avs = []
+	negative_avs = []
+	for antivirus, malware in report:
+	    antivirus = map(lambda x: x.encode("ascii", "ignore")
+			    if x is not None else None, antivirus)
+	    details = {
+		"av_name": antivirus[0],
+		"version": antivirus[1]
+	    }
+	    if malware is not None:
+		details["malware_name"] = malware.encode("ascii") 
+		positive_avs.append(details)
+	    else:
+		negative_avs.append(details)
+	report.avs_reporting_positive = positive_avs
+        report.avs_reporting_negative = negative_avs
+        return True
